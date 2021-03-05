c0098b38016c8c5b3b36bcd443834caf9e16064a
color-kurenawi <70028074+color-kurenawi@users.noreply.github.com>
2021-03-05 15:18:35 +0900

    Fixed pid controller

--- pid.cpp
+++ pid.cpp
@@ -4,27 +4,29 @@
 PID::PID() {}
 
 float PID::calc_angular_velocity(float yaw){
-    return (yaw - old_yaw) / ((float)SAMPLING_INTERVAL/1000);
+    float a_vel = (yaw - old_yaw) / ((float)SAMPLING_INTERVAL/1000);
+    if (a_vel <= -180) a_vel += 360;
+    if (a_vel >= 180) a_vel -= 360;
+    return a_vel;
 }
 
 void PID::calc_pid_elm(){
-    for (int i=0; i<4; i++) {
+    for (int i=0; i<3; i++) {
         pid_d[i] = (pid_p[i] - old_pid_p[i]) / ((float)SAMPLING_INTERVAL/1000);
         pid_i[i] += pid_p[i];
         old_pid_p[i] = pid_p[i];
     }
 }
 
-void PID::get_pid_res(float data[4]){
-    for (int i = 0; i < 4; ++i) data[i] = pid_res[i];
+void PID::get_pid_res(float data[3]){
+    for (int i = 0; i < 3; ++i) data[i] = pid_res[i];
 }
 
-void PID::update_pid(float RPY_data[3], float v_vel, float command_var[4]){
-    for(int i=0; i<2; i++) pid_p[i] = command_var[i] - RPY_data[i];
+void PID::update_pid(float RPY_data[3], float command_var[4]){
+    for(int i=0; i<2; i++) pid_p[i] = RPY_data[i] - command_var[i];
     float a_vel = calc_angular_velocity(RPY_data[2]);
     pid_p[2] = command_var[2] - a_vel;
-    pid_p[3] = command_var[3] - v_vel;
 
     calc_pid_elm();
-    for (int i = 0; i < 4; ++i) pid_res[i] = Kp[i]*pid_p[i] + Ki[i]*pid_i[i] + Kd[i]*pid_d[i];
+    for (int i = 0; i < 3; ++i) pid_res[i] = Kp[i]*pid_p[i] + Ki[i]*pid_i[i] + Kd[i]*pid_d[i];
 }
--- pid.h
+++ pid.h
@@ -9,21 +9,21 @@ class PID{
 
     // v_vel = vertical velocity
     // a_vel = angular velocity
-    void update_pid(float RPY_data[3], float v_vel, float command_var[4]);
+    void update_pid(float RPY_data[3], float command_var[4]);
     void calc_pid();
-    void get_pid_res(float data[4]);
+    void get_pid_res(float data[3]);
 
     private:
-    float Kp[4] = {10.0f, 10.0f, 10.0f, 10.0f};
-    float Ki[4] = {0.1f, 0.1f, 0.1f, 0.1f};
-    float Kd[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+    float Kp[3] = {10.0f, 10.0f, 10.0f};
+    float Ki[3] = {0.1f, 0.1f, 0.1f};
+    float Kd[3] = {1.0f, 1.0f, 1.0f};
     
-    float pid_p[4] = {0.0f, 0.0f, 0.0f, 0.0f};
-    float pid_i[4] = {0.0f, 0.0f, 0.0f, 0.0f};
-    float pid_d[4] = {0.0f, 0.0f, 0.0f, 0.0f};
-    float old_pid_p[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+    float pid_p[3] = {0.0f, 0.0f, 0.0f};
+    float pid_i[3] = {0.0f, 0.0f, 0.0f};
+    float pid_d[3] = {0.0f, 0.0f, 0.0f};
+    float old_pid_p[3] = {0.0f, 0.0f, 0.0f};
 
-    float pid_res[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+    float pid_res[3] = {0.0f, 0.0f, 0.0f};
 
     float old_yaw = 0.0f;
     float calc_angular_velocity(float yaw);
